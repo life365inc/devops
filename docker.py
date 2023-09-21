535ce25e138e0ce1f987fddc49194b7bbf72f78b
Kyle Thomas <kthomas852@yahoo.com>
2023-09-21 07:38:36 -0600

    Update docker.py

--- docker.py
+++ docker.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     temp_arr = section.split(":")
     secret_obj[temp_arr[0]] = temp_arr[1]
   raw_key = secret_obj["staging1_ssh_key"].replace("\n", "")
-  print(str(raw_key))
+  # print(str(raw_key))
   # Builds RSA key to be used in SSH
   my_key = f"""-----BEGIN RSA PRIVATE KEY-----
   {str(raw_key)}
   -----END RSA PRIVATE KEY-----"""
   pkey = paramiko.RSAKey.from_private_key(StringIO(my_key))
-
+  print(pkey)
   def pull(pkey, ip, image, tag):
     conn = Connection(
         host=f'{ip}',
