e5e6bac4bac741a0ca01dd023a759b26d4897f1a
Kyle Thomas <kthomas852@yahoo.com>
2023-09-20 22:58:06 -0600

    Create docker.py

---
+++ docker.py
@@ -0,0 +1,48 @@
+import sys
+import paramiko
+from io import StringIO
+
+if __name__ == "__main__":
+  # Builds RSA key to be used in SSH
+  my_key = f"""-----BEGIN RSA PRIVATE KEY-----
+  {sys.argv[3]}
+  -----END RSA PRIVATE KEY-----"""
+  pkey = paramiko.RSAKey.from_private_key(StringIO(my_key))
+
+  def pull(pkey, ip, image, tag):
+    conn = Connection(
+        host=f'{ip}',
+        user="ubuntu",
+        connect_kwargs={
+            "pkey": pkey,
+        },
+    )
+    try:
+        print(f"......pulling docker image {image}......")
+        conn.run(f"docker pull life365inc/{image}:{tag}")
+        conn.close()
+    except:
+        raise ValueError("Unable to execute")
+
+
+  def deploy(pkey, ip, image):
+    conn = Connection(
+        host=f'{ip}',
+        user="ubuntu",
+        connect_kwargs={
+            "pkey": pkey,
+        },
+    )
+    try:
+        print(f"......deploying {image}......")
+        conn.run(f"~/l365/l365 start")
+        conn.close()
+    except:
+        raise ValueError("Unable to execute")
+
+
+  if sys.argv[1] == "pull":
+    pull(pkey, sys.argv[2], sys.argv[4], sys.argv[5])
+  else:
+    deploy(pkey, sys.argv[2], sys.argv[4])
+    
